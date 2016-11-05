1ababc9a2a511422ec4f2deaefd4b7a9924ea6db
AzureAD\TorbenGoetzsche <torben@t1tech-dubai.com>
2016-11-05 15:19:03 +0100

    Added C# project for communicating with Robot and holding a robot model

---
+++ URConnect/RobotServer/RealTimeClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotServer
+{
+    class RealTimeClient
+    {
+        public RealTimeClient(IPAddress ipAddress, string password)
+        {
+            
+        }
+    }
+}
---
+++ URConnect/RobotServer/RobotController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotServer
+{
+    public class RobotController
+    {
+        private static RobotModel _robotModel;
+        private RobotConnector _robotConnector;
+
+        public RobotController()
+        {
+            _robotModel = new RobotModel();
+            _robotConnector = new RobotConnector(_robotModel);
+            Debug.WriteLine("I'm back!");
+        }
+
+        public string GetActualDigitalOutputBits()
+        {
+            return _robotModel.ActualDigitalOutputBits.ToString();
+            //return _robotModel.RobotTimestamp.ToString();
+        }
+
+
+    }
+}
