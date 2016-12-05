25262eda961b37e719772e9ffc14a836164a21be
AzureAD\TorbenGoetzsche <torben@t1tech-dubai.com>
2016-12-05 08:23:40 +0100

    Minor cleanup in robot connector

--- URConnect/UniversalRobotsConnect/RealTimeClient/RealTimeClientSender.cs
+++ URConnect/UniversalRobotsConnect/RealTimeClient/RealTimeClientSender.cs
@@ -12,9 +12,9 @@ namespace UniversalRobotsConnect
 {
     class RealTimeClientSender
     {
-        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        //private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        ConcurrentQueue<byte[]> _dataToSend = new ConcurrentQueue<byte[]>();
+        //ConcurrentQueue<byte[]> _dataToSend = new ConcurrentQueue<byte[]>();
         private NetworkStream _stream;
         private Thread _thread;
 
