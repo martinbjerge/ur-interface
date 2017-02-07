9831d7316556f0749eb1ea42eb0e482c5ea37340
Martin Huus Bjerge <martin@roperobotics.com>
2017-02-07 09:51:23 +0100

    Make MIB driver run in own thred and update robot data model.

--- URplus/mib.py
+++ URplus/mib.py
@@ -33,7 +33,7 @@ from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 import numpy as np
 
 
-class Mib(object):
+class Mib(threading.Thread):
     '''
     Driver for controlling DEIF MIB 7000C electical multi meter Modbus serial
     
@@ -42,20 +42,32 @@ class Mib(object):
     mib = URplus.mib.Mib(host='COM3')
     print(mib.GetVoltage())
     
-    
     Note: If using BarinBoxes ES-313 Ethernet to serial device, 
     remember to remove the tag in port default settings that ignores the application settings.
     http://[IP]/serialport1.html
     '''
     
     
-    def __init__(self, host):
+    def __init__(self, host, robotModel=None):
         '''
         Constructor - takes the serial port of the sander
         '''
+        threading.Thread.__init__(self)
         logger = URBasic.dataLogging.DataLogging()
         name = logger.AddEventLogging(__name__,log2Consol=False)        
         self.__logger = logger.__dict__[name]
+        
+        if(False):
+            assert isinstance(robotModel, URBasic.robotModel.RobotModel)  ### This line is to get code completion for RobotModel
+        if robotModel is None:
+            self.__robotModel = URBasic.robotModel.RobotModel()
+        else:
+            self.__robotModel = robotModel
+        self.__initDataModel()
+        
+        if host is None:
+            return
+
         self.client = ModbusClient(port=host, baudrate='19200', parity='N', stopbits=1, method='RTU')
         self.__serialUnit = 17
         self.__PT1 = 400
@@ -63,25 +75,52 @@ class Mib(object):
         self.__CT1 = 12
         self.__CT2 = 5
         self.__stopRunningFlag = False
+        self.__stop_event = True
+        self.start()
+        self.__logger.info('Bib constructor done')
+
         
     def close(self):
-        pass
-        
+        self.__stop_event = True
+        if self.isAlive():
+            self.join()
+            self.client.close()
 
+    
+    def run(self):
+        self.__stop_event = False
+        while not self.__stop_event:
+            data = np.array(self.client.read_holding_registers(304, 14, unit=self.__serialUnit).registers)
+            self.__robotModel.dataDir['mibGridFrequency'] = data[0]/100
+            self.__robotModel.dataDir['mibGridVoltge'] = data[1:7]*self.__PT1/self.__PT2/10
+            self.__robotModel.dataDir['mibGridCurrent'] = data[7:10]*self.__CT1/self.__CT2/1000 
+            self.__robotModel.dataDir['mibGridPower'] = data[11:14]*self.__PT1/self.__PT2*self.__CT1/self.__CT2
+            self.__robotModel.dataDir['mibGridEnergy'] = np.array(self.client.read_holding_registers(156, 1, unit=self.__serialUnit).registers)/10
+            self.__robotModel.dataDir['mibGridMaxCurrent'] = np.array(self.client.read_holding_registers(1126, 3, unit=self.__serialUnit).registers)*self.__CT1/self.__CT2/1000
+    
     def GetFrequency(self):
-        return np.array(self.client.read_holding_registers(304, 1, unit=self.__serialUnit).registers)/100
-
+        return self.__robotModel.dataDir['mibGridFrequency']
+    
     def GetVoltage(self):
-        return np.array(self.client.read_holding_registers(305, 6, unit=self.__serialUnit).registers)*self.__PT1/self.__PT2/10
-
+        return self.__robotModel.dataDir['mibGridVoltge']
+    
     def GetCurrent(self):
-        return np.array(self.client.read_holding_registers(311, 3, unit=self.__serialUnit).registers)*self.__CT1/self.__CT2/1000
-
+        return self.__robotModel.dataDir['mibGridCurrent']
+    
     def GetPower(self):
-        return np.array(self.client.read_holding_registers(315, 3, unit=self.__serialUnit).registers)*self.__PT1/self.__PT2*self.__CT1/self.__CT2
-
+        return self.__robotModel.dataDir['mibGridPower']
+    
     def GetEnergy(self):
-        return np.array(self.client.read_holding_registers(156, 1, unit=self.__serialUnit).registers)/10
+        return self.__robotModel.dataDir['mibGridEnergy']
     
     def GetMaxCurrent(self):
-        return np.array(self.client.read_holding_registers(1126, 3, unit=self.__serialUnit).registers)*self.__CT1/self.__CT2/1000
+        return self.__robotModel.dataDir['mibGridMaxCurrent']
+    
+    def __initDataModel(self):
+        self.__robotModel.dataDir['mibGridFrequency'] = None
+        self.__robotModel.dataDir['mibGridVoltge'] = None
+        self.__robotModel.dataDir['mibGridCurrent'] = None
+        self.__robotModel.dataDir['mibGridPower'] = None
+        self.__robotModel.dataDir['mibGridEnergy'] = None
+        self.__robotModel.dataDir['mibGridMaxCurrent'] = None
+        
