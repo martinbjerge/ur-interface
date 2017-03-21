a79f7048cdeb1e2291af385cad2817c759011626
Felix Blix Everberg <felix@roperobotics.com>
2017-03-21 11:18:09 +0100

    replaced abspath with os.path.join(re.split('\\|/',str)) to format paths

--- URBasic/dataLogging.py
+++ URBasic/dataLogging.py
@@ -2,23 +2,23 @@
 Python 3.x library to control an UR robot through its TCP/IP interfaces
 Copyright (C) 2017  Martin Huus Bjerge, Rope Robotics ApS, Denmark
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-and associated documentation files (the "Software"), to deal in the Software without restriction, 
-including without limitation the rights to use, copy, modify, merge, publish, distribute, 
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
 is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies 
+The above copyright notice and this permission notice shall be included in all copies
 or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL "Rope Robotics ApS" BE LIABLE FOR ANY CLAIM, 
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL "Rope Robotics ApS" BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-Except as contained in this notice, the name of "Rope Robotics ApS" shall not be used 
-in advertising or otherwise to promote the sale, use or other dealings in this Software 
+Except as contained in this notice, the name of "Rope Robotics ApS" shall not be used
+in advertising or otherwise to promote the sale, use or other dealings in this Software
 without prior written authorization from "Rope Robotics ApS".
 '''
 
@@ -27,8 +27,9 @@ __copyright__ = "Copyright 2017, Rope Robotics ApS, Denmark"
 __license__ = "MIT License"
 
 import logging
-import time 
+import time
 import os
+import re
 import URBasic
 import xml.etree.ElementTree as ET
 import ast
@@ -46,25 +47,25 @@ class DataLogging(with_metaclass(Singleton, object)):
     '''
     A module that add general logging functions to the UR Interface framework.
     '''
-    
+
     def __init__(self,path=None):
         '''
         Constructor that setup a path where log files will be stored.
         '''
         self.directory = None
         self.logDir = None
-        
+
         self.__developerTestingFlag = False
         self.__eventLogFileMode = 'w'
         self.__dataLogFileMode = 'w'
-        
+
         configFilename = URBasic.__file__[0:URBasic.__file__.find('URBasic')] + 'logConfig.xml'
         self.__readConfig(configFileName=configFilename)
-        
+
         self.GetLogPath(path=path, developerTestingFlag=self.__developerTestingFlag)
-        
-        
-        
+
+
+
         self.fileLogHandler = logging.FileHandler(self.directory + '\\UrEvent.log', mode=self.__eventLogFileMode)
         self.fileLogHandler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
         self.streamLogHandler = logging.StreamHandler()
@@ -73,13 +74,13 @@ class DataLogging(with_metaclass(Singleton, object)):
         self.writeDataLogHeadder = True
 
 
-    
+
     def __readConfig(self, configFileName):
         tree = ET.parse(configFileName)
         logConfig = tree.getroot()
         developerModeTag = logConfig.find('developerMode')
         self.__developerTestingFlag = ast.literal_eval(developerModeTag.text)
-                
+
         eventLogConfig = logConfig.find('eventLogConfig')
         eventFileModeTag = eventLogConfig.find('fileMode')
         if (eventFileModeTag.text == "Overwrite"):
@@ -88,7 +89,7 @@ class DataLogging(with_metaclass(Singleton, object)):
             self.__eventLogFileMode = 'a'
         else:
             raise ValueError("Not supported eventLogfile mode: " + eventFileModeTag.text)
-        
+
         dataLogConfig = logConfig.find('dataLogConfig')
         dataFileModeTag = dataLogConfig.find('fileMode')
         if (dataFileModeTag.text == "Overwrite"):
@@ -97,9 +98,9 @@ class DataLogging(with_metaclass(Singleton, object)):
             self.__dataLogFileMode = 'a'
         else:
             raise ValueError("Not supported dataLogfile mode: " + dataFileModeTag.text)
-        
-        
-        
+
+
+
     def GetLogPath(self,path=None, developerTestingFlag=True):
         '''
         Setup a path where log files will be stored
@@ -110,30 +111,30 @@ class DataLogging(with_metaclass(Singleton, object)):
         if path[-1:]=='\\':
             path = path[0:-1]
         if self.directory is None:
-            self.logDir = os.path.abspath(path)
+            self.logDir = os.path.join(re.split('\\\\|/', path))
             if developerTestingFlag:
                 self.directory = path
             else:
                 self.directory =  time.strftime(path + "\\%Y-%m-%d\\%H-%M-%S", time.localtime())
             if not os.path.exists(self.directory):
                 os.makedirs(self.directory)
-        self.directory = os.path.abspath(self.directory)
-        return self.directory, self.logDir        
-    
+        self.directory = os.path.join(re.split('\\\\|/',self.directory))
+        return self.directory, self.logDir
+
     def AddEventLogging(self, name='root', log2file=True, log2Consol=True, level = logging.INFO):
         '''
         Add a new event logger, the event logger can log data to a file and also output the log to the console.
-        
+
         Input Parameters:
         Name (str): The name of the logger the logger name will get the extension event
         Log2file (bool): Set if the log should be stored in a log file
-        Log2Consol (bool): Set if the log should be output to the console 
-        
+        Log2Consol (bool): Set if the log should be output to the console
+
         Return parameter:
         Name (str): The logger name including the extension
         '''
         name = name.replace('__', '').replace('.', '_') + 'Event'
-        self.__dict__[name] = logging.getLogger(name) 
+        self.__dict__[name] = logging.getLogger(name)
         if log2file:
             self.__dict__[name].addHandler(self.fileLogHandler)
         if log2Consol:
@@ -147,7 +148,7 @@ class DataLogging(with_metaclass(Singleton, object)):
 
         Input Parameters:
         Name (str): The name of the logger the logger name will get the extension Data
-        
+
         Return parameter:
         Name (str): The logger name including the extension
         '''
@@ -157,8 +158,7 @@ class DataLogging(with_metaclass(Singleton, object)):
         self.__dict__[name].setLevel(logging.INFO)
         if self.writeDataLogHeadder:
             self.__dict__[name].info('Time;ModuleName;Level;Channel;UR_Time;Value1;Value2;Value3;Value4;Value5;Value6')
-            self.fileDataLogHandler.setFormatter(logging.Formatter('%(asctime)s;%(name)s;%(levelname)s;%(message)s'))        
+            self.fileDataLogHandler.setFormatter(logging.Formatter('%(asctime)s;%(name)s;%(levelname)s;%(message)s'))
             self.__dict__[name].addHandler(self.fileDataLogHandler)
             self.writeDataLogHeadder = False
         return name
-    
