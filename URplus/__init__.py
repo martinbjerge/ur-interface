ee22f3f7eeca542d1eab2c4fde057e86c2aa6a77
Steffen Jacobsen <steffen@roperobotics.com>
2017-05-30 13:39:17 +0200

    Added coding to beckhoff and included files in urplus init

--- URplus/__init__.py
+++ URplus/__init__.py
@@ -33,3 +33,4 @@ from URplus.mib import Mib
 #from URplus.mis341 import MIS341
 from URplus.airosSander import AirosSander
 from URplus.xsensImu import XsensImu
+from URplus.beckhoff import BECKHOFF
