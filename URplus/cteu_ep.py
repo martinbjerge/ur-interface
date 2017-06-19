4c76ff00c7d783e4221dfcd8666d1fa258a510fb
Mikkel W. Jürgensen <mikkel@roperobotics.com>
2017-06-19 19:02:02 +0200

    fix to valve block

--- URplus/cteu_ep.py
+++ URplus/cteu_ep.py
@@ -2,23 +2,23 @@
 Python 3.x library to control an UR robot through its TCP/IP interfaces
 Copyright (C) 2017  Martin Huus Bjerge, Rope Robotics ApS, Denmark
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-and associated documentation files (the "Software"), to deal in the Software without restriction, 
-including without limitation the rights to use, copy, modify, merge, publish, distribute, 
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
 is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies 
+The above copyright notice and this permission notice shall be included in all copies
 or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL "Rope Robotics ApS" BE LIABLE FOR ANY CLAIM, 
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL "Rope Robotics ApS" BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-Except as contained in this notice, the name of "Rope Robotics ApS" shall not be used 
-in advertising or otherwise to promote the sale, use or other dealings in this Software 
+Except as contained in this notice, the name of "Rope Robotics ApS" shall not be used
+in advertising or otherwise to promote the sale, use or other dealings in this Software
 without prior written authorization from "Rope Robotics ApS".
 '''
 
@@ -39,10 +39,10 @@ class CTEU_EP(object):
         '''
         Constructor - takes ip address to the CTEU-EP box
         '''
-        self.__connectionState = URBasic.connectionState.ConnectionState.DISCONNECTED        
-        
-        logger = URBasic.dataLogging.DataLogging()        
-        name = logger.AddEventLogging(__name__)        
+        self.__connectionState = URBasic.connectionState.ConnectionState.DISCONNECTED
+
+        logger = URBasic.dataLogging.DataLogging()
+        name = logger.AddEventLogging(__name__)
         self.__logger = logger.__dict__[name]
 
         if host is None: #Only for enable code completion
@@ -51,19 +51,19 @@ class CTEU_EP(object):
         if(False):
             assert isinstance(robotModel, URBasic.robotModel.RobotModel)  ### This line is to get code completion for RobotModel
         self.__robotModel = robotModel
-        
+
         self.__client = ModbusClient(host=host)
         connected = self.__client.connect()
         if(connected):
             self.__logger.info("Modbus connected to CTEU-EP")
-            self.__connectionState = URBasic.connectionState.ConnectionState.CONNECTED        
+            self.__connectionState = URBasic.connectionState.ConnectionState.CONNECTED
 
         else:
             self.__connectionState = URBasic.connectionState.ConnectionState.ERROR
-            
-        
+
+
     def setValve(self, valveNumber, state):
-        
+
         '''
         Set a valve - 0 to 23 to True or False
         '''
@@ -72,14 +72,19 @@ class CTEU_EP(object):
         if not self.__connectionState>URBasic.connectionState.ConnectionState.DISCONNECTED:
             self.__logger.warning('SetValve but not Connected')
             return
-             
+
         if self.__robotModel.StopRunningFlag():
             return
         result = self.__client.write_coil(valveNumber, state)
-        if(result == None): #Just one retry
-            time.sleep(0.2)
-            result = self.__client.write_coil(valveNumber, state)
-        
+        for nr in range(0,10):
+            if(result == None): #Just one retry
+                time.sleep(0.2)
+                result = self.__client.write_coil(valveNumber, state)
+            else:
+                return True
+        self.__logger.warning('Valve not turned')
+        return False
+
     def getValvePosition(self, valveNumber):
         '''
         Get the state of a valve - 0 to 23
@@ -98,5 +103,3 @@ class CTEU_EP(object):
             return result.bits[0]
         else:
             return None
-        
-        
